5ff4d363a9d607d32812d5c16e30cb661a04ab2c
ManithMam <manithmam@DESKTOP-HHMD9GT>
2024-05-14 18:11:35 +0900

    Implement cell class

---
+++ cell.cpp
@@ -0,0 +1,40 @@
+#include <SFML/Graphics.hpp>
+#include "cell.hpp"
+
+const sf::Color white = sf::Color(255, 255, 255);
+const sf::Color black = sf::Color(0, 0, 0);
+const sf::Color red = sf::Color(255, 0, 0);
+const sf::Color yellow = sf::Color(255, 255, 0);
+
+cell::Cell::Cell(x_y_position cell_position_, int window_x_pos, int window_y_pos) {
+
+	populated = false;	
+	populated_color = yellow;
+	color = white;
+	this->setFillColor(color);
+	this->setOutlineThickness(2.f);
+	this->setOutlineColor(red);
+	this->setPosition(window_x_pos, window_y_pos);	
+	this->setSize(sf::Vector2f(25.f, 25.f));
+	cell_position = cell_position_;
+}
+
+void cell::Cell::set_populated() {
+	this->populated = true;
+	this->color = yellow;
+}
+
+void cell::Cell::set_unpopulated() {
+	this->populated = false;
+	this->color = white;	
+}
+
+cell::x_y_position cell::Cell::get_position() {
+	return this->cell_position;
+}
+
+bool cell::Cell::get_populated() {
+	return this->populated;
+}
+
+
---
+++ cell.hpp
@@ -0,0 +1,37 @@
+#ifndef CELL_H
+#define CELL_H
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+namespace cell {
+
+	struct x_y_position {
+		int x_position;
+		int y_position;
+	};
+
+	class Cell : public sf::RectangleShape {
+
+	private:
+
+		bool populated;
+		sf::Color color;
+		sf::Color populated_color;
+		x_y_position cell_position;
+
+		void set_populated();
+		void set_unpopulated();
+
+	public:
+
+		x_y_position get_position();
+		bool get_populated();
+
+		Cell(x_y_position cell_position_, int window_x_pos, int window_y_pos);
+
+	};
+}
+
+#endif
+
