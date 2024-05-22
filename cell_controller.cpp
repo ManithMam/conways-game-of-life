c93817b752a2ef0df318fc80b1d897e6a42bb55f
ManithMam <manithmam@DESKTOP-HHMD9GT>
2024-05-22 20:25:27 +0900

    Implement get_populated_cells method

--- cell_controller.cpp
+++ cell_controller.cpp
@@ -39,7 +39,61 @@ void cell_ctr::Cell_Controller::cell_click(std::vector<std::vector<cell::Cell>>&
 
 }
 
-void calculate_neighbour_count(int& neigbours, std::vector<std::vector<cell::Cell>>& cells, const cell::neigbouring_positions& pos) {
+void calculate_neighbour_count(int& neigbours, const cell::neigbouring_positions& pos, std::map<cell::x_y_position, cell::Cell, cell::x_y_comp>& populated_cells) {
+
+	//upper_left
+	if (populated_cells.find({ pos.upper_left.x_position, pos.upper_left.y_position }) != populated_cells.end()) {		
+		neigbours++;
+	}
+
+	//above
+	if (populated_cells.find({ pos.above.x_position, pos.above.y_position }) != populated_cells.end()) {		
+		neigbours++;
+	}
+
+	//upper_right
+	if (populated_cells.find({ pos.upper_right.x_position, pos.upper_right.y_position }) != populated_cells.end()) {		
+		neigbours++;
+	}
+
+	//left
+	if (populated_cells.find({ pos.left.x_position, pos.left.y_position }) != populated_cells.end()) {		
+		neigbours++;
+	}
+
+	//right
+	if (populated_cells.find({ pos.right.x_position, pos.right.y_position }) != populated_cells.end()) {		
+		neigbours++;
+	}
+
+	//lower_left
+	if (populated_cells.find({ pos.lower_left.x_position, pos.lower_left.y_position }) != populated_cells.end()) {		
+		neigbours++;
+	}
+
+	//below
+	if (populated_cells.find({ pos.below.x_position, pos.below.y_position }) != populated_cells.end()) {		
+		neigbours++;
+	}
+
+	//lower_right
+	if (populated_cells.find({ pos.lower_right.x_position, pos.lower_right.y_position }) != populated_cells.end()) {		
+		neigbours++;
+	}
+}
+
+
+void set_cell_neighbourcount(cell::Cell& cell, std::vector<std::vector<cell::Cell>>& cells, const cell::neigbouring_positions& neighbour_pos, std::map<cell::x_y_position, cell::Cell, cell::x_y_comp>& populated_cells) {
+
+	int neighbours_amount = 0;	
+
+	calculate_neighbour_count(neighbours_amount, neighbour_pos, populated_cells);
+	
+	cell.set_num_of_neighbours(neighbours_amount);
+	
+}
+
+std::map<cell::x_y_position, cell::Cell, cell::x_y_comp> get_populated_cells(std::vector<std::vector<cell::Cell>>& cells) {
 
 	std::map<cell::x_y_position, cell::Cell, cell::x_y_comp> populated_cells;
 
@@ -47,85 +101,25 @@ void calculate_neighbour_count(int& neigbours, std::vector<std::vector<cell::Cel
 		for (cell::Cell cell : cell_vector) {
 			if (cell.get_populated() == true) {
 				populated_cells[cell.get_position()] = cell;
-			}			
+			}
 		}
-	}	
-
-	for (const auto el : populated_cells) {
-		std::cout << "Cell with position: " << el.first.x_position << "|" << el.first.y_position << "is alive" << '\n';
 	}
 
-	//upper_left
-	if (populated_cells.find({ pos.upper_left.x_position, pos.upper_left.y_position }) != populated_cells.end()) {
-		std::cout << "upper_left" << pos.upper_left.x_position << "|" << pos.upper_left.y_position << '\n';
-		neigbours++;
-	}
-
-	//above
-	if (populated_cells.find({ pos.above.x_position, pos.above.y_position }) != populated_cells.end()) {
-		std::cout << "above" << pos.above.x_position << "|" << pos.above.y_position << '\n';
-		neigbours++;
-	}
-
-	//upper_right
-	if (populated_cells.find({ pos.upper_right.x_position, pos.upper_right.y_position }) != populated_cells.end()) {
-		std::cout << "upper_right" << pos.upper_right.x_position << "|" << pos.upper_right.y_position << '\n';
-		neigbours++;
-	}
-
-	//left
-	if (populated_cells.find({ pos.left.x_position, pos.left.y_position }) != populated_cells.end()) {
-		std::cout << "left" << pos.left.x_position << "|" << pos.left.y_position << '\n';
-		neigbours++;
-	}
-
-	//right
-	if (populated_cells.find({ pos.right.x_position, pos.right.y_position }) != populated_cells.end()) {
-		std::cout << "right" << pos.right.x_position << "|" << pos.right.y_position << '\n';
-		neigbours++;
-	}
-
-	//lower_left
-	if (populated_cells.find({ pos.lower_left.x_position, pos.lower_left.y_position }) != populated_cells.end()) {
-		std::cout << "lowere_left" << pos.lower_left.x_position << "|" << pos.lower_left.y_position << '\n';
-		neigbours++;
-	}
-
-	//below
-	if (populated_cells.find({ pos.below.x_position, pos.below.y_position }) != populated_cells.end()) {
-		std::cout << "below" << pos.below.x_position << "|" << pos.below.y_position << '\n';
-		neigbours++;
-	}
-
-	//lower_right
-	if (populated_cells.find({ pos.lower_right.x_position, pos.lower_right.y_position }) != populated_cells.end()) {
-		std::cout << "lower_right" << pos.lower_right.x_position << "|" << pos.lower_right.y_position << '\n';
-		neigbours++;
-	}
-}
-
-
-void set_cell_neighbourcount(cell::Cell& cell, std::vector<std::vector<cell::Cell>>& cells, const cell::neigbouring_positions& neighbour_pos) {
-
-	int neighbours_amount = 0;
-
-	if (cell.get_populated() == true) {
-		
-		calculate_neighbour_count(neighbours_amount, cells, neighbour_pos);
-
-		std::cout << "Cell: " << cell.get_position().x_position << " | " << cell.get_position().y_position << "has, " << neighbours_amount << " neighbours" << '\n';
-		cell.set_num_of_neighbours(neighbours_amount);
-	}
+	return populated_cells;
 }
 
 void cell_ctr::Cell_Controller::check_neighbours(std::vector<std::vector<cell::Cell>>& cells) {
 
+	std::map<cell::x_y_position, cell::Cell, cell::x_y_comp> populated_cells;
+
+	populated_cells = get_populated_cells(cells);
+
 	for (int i = 0; i < cells.size(); i++) {
 		for (int t = 0; t < cells[i].size(); t++) {
-			cell::Cell cell = cells[i][t];
+			cell::Cell& cell = cells[i][t];
 			cell::neigbouring_positions neighbour_pos = cell.get_neighbour_pos();	
 
-			set_cell_neighbourcount(cell, cells, neighbour_pos);
+			set_cell_neighbourcount(cell, cells, neighbour_pos, populated_cells);
 		}
 	}
 
